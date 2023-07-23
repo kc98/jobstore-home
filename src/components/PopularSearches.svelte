4c40458c681f7ea6cd50ce02b0e9a064498cec63
kc98 <37565368+kc98@users.noreply.github.com>
2023-07-24 00:47:02 +0800

    Implement popular search

---
+++ src/components/PopularSearches.svelte
@@ -0,0 +1,95 @@
+<div class="max-w-[80%] mx-auto my-0">
+  <div class="text-center w-full text-[1.5rem] font-light mb-4 text-[#515357]">
+    Popular Searches
+  </div>
+  <div class="grid grid-flow-row auto-rows-auto">
+    <div class="grid grid-flow-col auto-cols-auto">
+      <div class="category">Popular categories</div>
+      <div>
+        <ul>
+          <li><a href="/#">Sales / Marketing jobs</a></li>
+          <li><a href="/#">Accounting / Auditing jobs</a></li>
+          <li><a href="/#">Administrative / Clerical jobs</a></li>
+          <li><a href="/#">Engineering jobs</a></li>
+          <li><a href="/#">Information Technology jobs</a></li>
+          <li><a href="/#">F&B / Tourism / Hospitality jobs</a></li>
+          <li><a href="/#">Customer Service jobs</a></li>
+          <li><a href="/#">View all categories</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <div class="grid grid-flow-col auto-cols-auto">
+    <div class="category">Popular locations</div>
+    <div>
+      <li><a href="/#">Kuala Lumpur</a></li>
+      <li><a href="/#">Petaling Jaya</a></li>
+      <li><a href="/#">Shah Alam</a></li>
+      <li><a href="/#">Johor Bahru</a></li>
+      <li><a href="/#">Puchong</a></li>
+      <li><a href="/#">Subang Jaya</a></li>
+      <li><a href="/#">Klang</a></li>
+      <li><a href="/#">Selangor</a></li>
+      <li><a href="/#">Seri Kembangan</a></li>
+      <li><a href="/#">Penang</a></li>
+      <li><a href="/#">View all locations</a></li>
+    </div>
+  </div>
+
+  <!-- <div class="grid grid-flow-col auto-cols-auto">
+    <div class="grid grid-flow-row auto-rows-auto">
+      <div class="category">Popular categories</div>
+      <div class="category">Popular locations</div>
+    </div>
+    <div class="grid grid-flow-row auto-rows-auto">
+      <div>
+        <ul>
+          <li><a href="/#">Sales / Marketing jobs</a></li>
+          <li><a href="/#">Accounting / Auditing jobs</a></li>
+          <li><a href="/#">Administrative / Clerical jobs</a></li>
+          <li><a href="/#">Engineering jobs</a></li>
+          <li><a href="/#">Information Technology jobs</a></li>
+          <li><a href="/#">F&B / Tourism / Hospitality jobs</a></li>
+          <li><a href="/#">Customer Service jobs</a></li>
+          <li><a href="/#">View all categories</a></li>
+        </ul>
+      </div>
+      <div>
+        <li><a href="/#">Kuala Lumpur</a></li>
+        <li><a href="/#">Petaling Jaya</a></li>
+        <li><a href="/#">Shah Alam</a></li>
+        <li><a href="/#">Johor Bahru</a></li>
+        <li><a href="/#">Puchong</a></li>
+        <li><a href="/#">Subang Jaya</a></li>
+        <li><a href="/#">Klang</a></li>
+        <li><a href="/#">Selangor</a></li>
+        <li><a href="/#">Seri Kembangan</a></li>
+        <li><a href="/#">Penang</a></li>
+        <li><a href="/#">View all locations</a></li>
+      </div>
+    </div>
+  </div> -->
+</div>
+
+<style lang="postcss">
+  .category {
+    @apply mr-2;
+    @apply mb-10;
+    @apply justify-self-stretch;
+  }
+
+  li {
+    @apply inline;
+    @apply mr-3;
+    @apply text-[#0704e1];
+    @apply text-sm;
+  }
+
+  li:last-child {
+    @apply text-[#333];
+  }
+
+  a:hover {
+    @apply text-[#333];
+  }
+</style>
