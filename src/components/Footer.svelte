a66b2bcc3482f01fd219dcbf1f7c945807f4e9f1
kc98 <37565368+kc98@users.noreply.github.com>
2023-07-24 00:45:57 +0800

    Implement Footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,137 @@
+<script lang="ts">
+</script>
+
+<div
+  class="w-full border-t-[1px] pt-8 mt-8 pb-6 text-sm bg-[#f8f8f8] justify-center flex"
+>
+  <div class="w-[75%]">
+    <img
+      class="mx-auto h-[33px]"
+      src="/images/jobstore_logo_blue.png"
+      alt="logo"
+    />
+    <div class="mt-12 mx-20">
+      <div class="grid grid-flow-col auto-cols-fr justify-center">
+        <div>
+          <div class="category-title">COMPANY</div>
+          <ul>
+            <li><a href="/#">About Us</a></li>
+            <li><a href="/#">Contact Support</a></li>
+            <li><a href="/#">Careers @ Jobstore</a></li>
+            <li><a href="/#">Blog</a></li>
+            <li><a href="/#">Partner Sites</a></li>
+            <li><a href="/#">Fulfillment Policy</a></li>
+          </ul>
+        </div>
+        <div>
+          <div class="category-title">JOBSEEKER</div>
+          <ul>
+            <li><a href="/#">Sign Up</a></li>
+            <li><a href="/#">Sign In</a></li>
+            <li><a href="/#">Check Applications</a></li>
+            <li><a href="/#">Jobseeker FAQs</a></li>
+            <li><a href="/#">Browse Jobs</a></li>
+            <li><a href="/#">Browse by Industry</a></li>
+          </ul>
+        </div>
+        <div>
+          <div class="category-title">EMPLOYER</div>
+          <ul>
+            <li><a href="/#">Create Account</a></li>
+            <li><a href="/#">Post Jobs</a></li>
+            <li><a href="/#">Products & Prices</a></li>
+            <li><a href="/#">Customer Support</a></li>
+            <li><a href="/#">Contact Sales</a></li>
+          </ul>
+        </div>
+        <div>
+          <div class="category-title">CONNECT WITH US</div>
+          <ul>
+            <li>
+              <img
+                src="https://asset.jobstore.com/images/index_icons/icon_fb_grey.png"
+                alt="Facebook icon"
+                class="contact-icon"
+              /><a href="/#">Facebook</a>
+            </li>
+            <li>
+              <img
+                src="https://asset.jobstore.com/images/index_icons/icon_twt_grey.png"
+                alt="Twitter icon"
+                class="contact-icon"
+              /><a href="/#">Twitter</a>
+            </li>
+            <li>
+              <img
+                src="https://asset.jobstore.com/images/index_icons/icon_in_grey.png"
+                alt="LinkedIn icon"
+                class="contact-icon"
+              /><a href="/#">LinkedIn</a>
+            </li>
+            <li>
+              <img
+                src="https://asset.jobstore.com/images/index_icons/icon_insta_grey.png"
+                alt="Instagram icon"
+                class="contact-icon"
+              />Instagram
+            </li>
+            <li><a href="/#">Call us at +603 2716 5199</a></li>
+            <li><a href="/#">Send us a message</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="flex justify-between mt-10">
+        <div>
+          <img
+            src="https://assets.jobstore.com/images/icon_locationpin.png"
+            alt="Country pin icon"
+            class="inline w-[13px] mr-1"
+          />
+          Malaysia
+          <img
+            src="https://assets.jobstore.com/images/arrow_up.png"
+            alt="Select country arrow"
+            class="inline w-[13px] ml-1"
+          />
+        </div>
+        <div>
+          &copy; 2023 Jobstore <span class="bullet-saperator">&bullet;</span>
+          <a href="/#">Terms of Service</a>
+          <span class="bullet-saperator">&bullet;</span>
+          <a href="/#">Privacy Policy</a>
+          <span class="bullet-saperator">&bullet;</span> All Rights Reserved
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<style lang="postcss">
+  div {
+    @apply text-[#444];
+  }
+
+  .category-title {
+    @apply text-[#0704e1];
+    @apply font-bold;
+    @apply mb-3;
+  }
+
+  .contact-icon {
+    @apply inline;
+    @apply w-[13px];
+    @apply mr-3;
+  }
+
+  .bullet-saperator {
+    @apply mx-3;
+  }
+
+  li {
+    @apply mb-1;
+  }
+
+  a:hover {
+    @apply text-[#0704e1];
+  }
+</style>
