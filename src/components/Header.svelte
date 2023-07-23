f5ab481ebcbfbf52e2437316a5eac8b2558c260e
kc98 <37565368+kc98@users.noreply.github.com>
2023-07-24 00:45:46 +0800

    Implement Header

---
+++ src/components/Header.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+</script>
+
+<div class="flex flex-row w-full justify-between p-10">
+  <div class="w-1/5">
+    <a href="/">
+      <img
+        class="max-w-[150px]"
+        src="/images/jobstore_logo_blue.png"
+        alt="logo"
+      />
+    </a>
+  </div>
+  <div class="flex justify-between w-3/5 items-center">
+    <div class="flex justify-between w-2/6">
+      <a class="font-bold text-grey text-[13px]" href="/">Browse Jobs</a>
+      <a class="font-bold text-grey text-[13px]" href="/">Companies</a>
+      <a class="font-bold text-grey text-[13px]" href="/">Download App</a>
+    </div>
+    <div class="flex justify-between w-1/5 mr-4">
+      <a class="font-bold text-grey text-[13px]" href="/">Sign In</a>
+      <a class="font-bold text-grey text-[13px]" href="/">Sign Up</a>
+    </div>
+  </div>
+  <a
+    class="bg-purple-blue py-2 px-10 rounded-full text-white text-[0.85rem]"
+    href="#"
+  >
+    <div>EMPLOYERS</div>
+  </a>
+</div>
