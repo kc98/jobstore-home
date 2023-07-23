089aab125c4da5b0d938970f63a9f171170d2e13
kc98 <37565368+kc98@users.noreply.github.com>
2023-07-24 00:46:38 +0800

    Implement Job list ui

---
+++ src/components/Job.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+</script>
+
+<div
+  class="shadow-[0_10px_15px_0px_rgb(238,238,238)] mr-8 last:mr-0 p-4 w-[312px] hover:shadow-[0px_10px_15px_0px_rgb(221,221,221)]"
+>
+  <div class="text-[#888] font-light text-[0.7rem]">Subang Jaya</div>
+  <div class="text-left">
+    <div class="font-bold text-[1.15rem]">
+      Manufacturing Manager (Engineer) – Steel/Aluminum Fabrication
+    </div>
+    <div class="text-[#0704e1] text-[0.8rem]">
+      MYR 10,000 - MYR 15,000 (Monthly)
+    </div>
+  </div>
+  <div class="flex justify-between">
+    <div>
+      <div class="text-[#888] font-light text-[0.7rem]">
+        Asia Tents Arena Sdn Bhd
+      </div>
+    </div>
+    <div class="box_job_logo w-[18%] h-[50px] max-w-[100px]">
+      <img
+        class="max-h-[90%] max-w-[95%]"
+        src="/images/jobstore_logo_blue.png"
+        alt="logo"
+      />
+    </div>
+  </div>
+</div>
---
+++ src/components/ThreeJobs.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import Job from "./Job.svelte";
+</script>
+
+<div class="flex justify-center w-[60%] my-4">
+  <Job />
+  <Job />
+  <Job />
+</div>
